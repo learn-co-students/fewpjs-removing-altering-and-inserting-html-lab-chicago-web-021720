8d2f8320cf4863561d1f90ebe79000dc18d5782b
ellisandrews <ellisandrews1@gmail.com>
2020-03-30 12:07:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+// Delete the <main id="main"> element
+document.querySelector('main#main').remove();
+
+// Create a new <h1> element
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Ellis is the champion!';
+
+// Add the new element to the <body>
+let body = document.querySelector('body');
+body.appendChild(newHeader);
