f594b5808bda887c5615a453f733d8fb01e50c60
cooljasonmelton <cooljasonmelton>
2020-03-30 17:13:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const element = document.querySelector('main#main')
+element.remove()
+
+let newElement = document.createElement('h1')
+newElement.id = 'victory'
+document.body.appendChild(newElement)
+
+
+
+let newHeader = document.querySelector('h1#victory')
+newHeader.innerHTML = "YOUR-NAME is the champion"
